abbad887e050063d1be4c7cc0f98fc1e05b748bd
Perseverance Mutengera <123206539+Persemutengera@users.noreply.github.com>
2023-07-27 07:38:13 +0200

    Update cd_shell.c

--- cd_shell.c
+++ cd_shell.c
@@ -14,8 +14,32 @@ void cd_dot(data_shell *datash)
 
 	getcwd(pwd, sizeof(pwd));
 	cp_pwd = _strdup(pwd);
-	set_env("OLDPWD", cp_pwd, datash);
-	dir = datash->args[1];
+	set_env("PWD", cp_pwd, datash);
+		free(cp_pwd);
+		return;
+	}
+	if (_strcmp("/", cp_pwd) == 0)
+	{
+		free(cp_pwd);
+		return;
+	}
+	cp_strtok_pwd = cp_pwd;
+	rev_string(cp_strtok_pwd);
+	cp_strtok_pwd = _strtok(cp_strtok_pwd, "/");
+	if (cp_strtok_pwd != NULL)set_env("OLDPWD", cp_pwd, datash);
+	dir = datash->args[1];set_env("PWD", cp_pwd, datash);
+		free(cp_pwd);
+		return;
+	}
+	if (_strcmp("/", cp_pwd) == 0)
+	{
+		free(cp_pwd);
+		return;
+	}
+	cp_strtok_pwd = cp_pwd;
+	rev_string(cp_strtok_pwd);
+	cp_strtok_pwd = _strtok(cp_strtok_pwd, "/");
+	if (cp_strtok_pwd != NULL)
 	if (_strcmp(".", dir) == 0)
 	{
 		set_env("PWD", cp_pwd, datash);
